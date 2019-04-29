5c9754a0478d29aa8870acbf590b628a1be9600d
Home <home@Grozdanovs-MacBook-Pro.local>
2019-04-30 01:37:17 +0300

    Rename chrome folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   entry: {
-    'background-page': join(__dirname, 'app/chrome/src/background-page.ts'),
-    'block-crawler': join(__dirname, 'app/chrome/src/block-crawler.ts'),
-    'dashboard-controller': join(__dirname, 'app/chrome/src/dashboard-controller.ts'),
-    'pool-info-crawler': join(__dirname, 'app/chrome/src/pool-info-crawler.ts')
+    'background-page': join(__dirname, 'app/inject-script/src/background-page.ts'),
+    'block-crawler': join(__dirname, 'app/inject-script/src/block-crawler.ts'),
+    'dashboard-controller': join(__dirname, 'app/inject-script/src/dashboard-controller.ts'),
+    'pool-info-crawler': join(__dirname, 'app/inject-script/src/pool-info-crawler.ts')
   },
   output: {
     path: join(__dirname, 'dist/mphfe'),
@@ -19,7 +19,7 @@ module.exports = {
       {
         exclude: /node_modules/,
         test: /\.ts?$/,
-        use: 'awesome-typescript-loader?{configFileName: "app/chrome/tsconfig.json"}'
+        use: 'awesome-typescript-loader?{configFileName: "app/inject-script/tsconfig.json"}'
       }
     ]
   },
